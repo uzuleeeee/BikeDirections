754811f8c545a950f803dbc952a3f60d7dd73c2b
uzuleeeee <ulee36@gatech.edu>
2026-02-24 09:59:42 -0500

    Initial Commit

---
+++ BikeDirections/BikeDirectionsApp.swift
@@ -0,0 +1,17 @@
+//
+//  BikeDirectionsApp.swift
+//  BikeDirections
+//
+//  Created by Mac-aroni on 2/24/26.
+//
+
+import SwiftUI
+
+@main
+struct BikeDirectionsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BikeDirections/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BikeDirections
+//
+//  Created by Mac-aroni on 2/24/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
